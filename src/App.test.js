623f857e476bc3e10860efacd95b3c9cc9b61ba2
Warren C. Longmire <warren.longmire@lambdaschool.com>
2022-01-13 13:05:37 -0500

    lecture work-in-progress

--- src/App.test.js
+++ src/App.test.js
@@ -13,12 +13,16 @@ test("loads header on mount", ()=> {
     //Act: find the header if it exists
 
     //Query: finds one element and returns null if not found
-    const header = screen.queryByText("Add New Animal");
+    const header = screen.queryAllByText("Add New Animal");
 
 
     //Get: finds one element and fails the test if not found
     // const header = screen.getByText("Add New Animal");
 
+
+    //Find: finds one element but returns a promise. Fail the test if not found
+    // const header = screen.findByText("Add New Animal");
+    
     //Assert: verify that the header exists
     // expect(header).toBeInTheDocument();
 });
