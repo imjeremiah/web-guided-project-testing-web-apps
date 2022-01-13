868abf8b681322178396f6adc1ef9075dcf8c01d
Warren C. Longmire <warren.longmire@lambdaschool.com>
2022-01-13 12:47:18 -0500

    lecture work-in-progress

--- src/App.test.js
+++ src/App.test.js
@@ -5,15 +5,16 @@
 //     console.log("test 1 stuff");
 // });
 
-
-test("Make sure things are work 1", ()=> {
-    console.log("test 1 stuff");
-});
-
-test("Make sure things are work 2", ()=> {
-    console.log("test 2 stuff");
-});
-
-test("Make sure things are work 3", ()=> {
-    console.log("test 3 stuff");
+describe("login", ()=> {
+    test("Make sure things are work 1", ()=> {
+        console.log("test 1 stuff");
+    });
+    
+    test("Make sure things are work 2", ()=> {
+        console.log("test 2 stuff");
+    });
+    
+    test("Make sure things are work 3", ()=> {
+        console.log("test 3 stuff");
+    });
 });
