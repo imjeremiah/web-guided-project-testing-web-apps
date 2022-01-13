bce0b8316c275c4904d4b97d5f650842e911dea9
Warren C. Longmire <warren.longmire@lambdaschool.com>
2022-01-13 13:37:32 -0500

    lecture work-in-progress

--- src/components/AnimalForm.test.js
+++ src/components/AnimalForm.test.js
@@ -10,7 +10,19 @@ test("renders without errors", ()=> {
 test("when user fills out all animal form inputs and submits, displays species", () => {
     //Arrange: render AnimalForm
     render(<AnimalForm/>);
+
+    //Act: Do a complete submission.
+    //  1. Select the species input.
+    const species = screen.getByLabelText(/species:/i);
+    console.log(species);
     
-    //Act:
-    //Assert:
+    //  2. Type in our species.
+    //  3. Select the age input.
+    //  4. Type in our age.
+    //  5. Select our notes.
+    //  6. Type in our notes.
+    //  7. click our submit button.
+
+    //Assert: 
+    //Shows our species name in current animals list.
 });
